bf4a57ab37ac93ca790a65eef58ded005464ad6f
hs <sth1pal@pal87498.local>
2015-04-06 19:20:27 -0700

    added example to overview

--- src/init/overview.js
+++ src/init/overview.js
@@ -6,7 +6,42 @@
  * @file angular factory for providing ...
  * @copyright Helpful Scripts 2015
  * @description
- * Provides *AngularJS* directives to ... 
+ * Provides *AngularJS* directives to facilitate {@link hsWidgets.directive:hsWidget widgets} on a {@link hsWidgets.directive:hsDashboard dashboard}. 
+ * 
+@example
+<example module="hsWidgets">
+    <file name="index.html">
+        <hs-dashboard hs-moveable>
+            <hs-widget hs-size='["50%", "50%"]'>
+                <br>1
+                <br>Move me along the top.
+                <br>Size me from the corners.
+            </hs-widget>
+            <div hs-widget hs-size='["50%", "50%"]'>
+                <br>2
+                <br>Move me along the top.
+                <br>Size me from the corners.
+            </div>
+            <div hs-widget hs-size='["100%", "50%"]'>
+                <br>3
+                <br>Move me along the top.
+                <br>Size me from the corners.
+            </div>
+        </hs-dashboard>
+    </file>
+    <file name="style.css">
+        .well           { position: relative; height: 300px; padding:0; }
+        hs-dashboard    { background-color: #eee; }
+        hs-widget>div, [hs-widget]>div { 
+            background-color: rgba(255, 255, 240, 0.75); 
+            text-align: center;
+        }
+        [hs-widget]>div { 
+            background-color: rgba(240, 240, 255, 0.75); 
+            text-align: center;
+        }
+    </file>
+</example>
 */
 
 
