0090a89f8126573da4a0001b1b343e57c2e72e9d
hs <sth1pal@pal87498>
2016-04-17 17:41:15 -0700

    fixed docs

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -1,137 +1,175 @@
 NG_DOCS={
   "sections": {
-    "api": "API Documentation"
+    "hs": "hs",
+    "hsLayout": "hsLayout"
   },
   "pages": [
     {
-      "section": "api",
+      "section": "hs",
+      "id": "index",
+      "shortName": "index",
+      "type": "overview",
+      "moduleName": "index",
+      "shortDescription": "HelpfulScripts foundational services",
+      "keywords": "angular angularjs base configuration dependencies dependency deprecation foundational helpful helpfulscripts higher hs hsconfigurable hsdeprecated hsobject https inheritance injection integration js message model modules object optional options overview registry scripts service services simple"
+    },
+    {
+      "section": "hs",
+      "id": "hs",
+      "shortName": "hs",
+      "type": "overview",
+      "moduleName": "hs",
+      "shortDescription": "hs",
+      "keywords": "$scope angular cities city controller dependency function hs hsobject html injection js length london mechanism module myctrl myobj ng-controller ng-repeat overview paris register registry return script service simple var"
+    },
+    {
+      "section": "hs",
       "id": "hs.hs",
       "shortName": "hs.hs",
       "type": "object",
       "moduleName": "hs",
       "shortDescription": "Provides a registry for modules, as well as a simpe injection mechanism that",
-      "keywords": "allows angular angular-style angularjs api array browser called calling dependencies element eliminates environments equivalent fashion fn function global hs injected injection injects invoke invokes mechanism module modules myexample myinstance mymodule myothermodule object parameters pattern register registered registers registry required result simpe usage var"
+      "keywords": "allows angular angular-style angularjs array browser called calling dependencies element eliminates environments equivalent fashion fn function global hs injected injection injects invoke invokes mechanism module modules myexample myinstance1 myinstance2 mymodule myothermodule object parameters pattern register registered registers registry required result simpe usage var"
     },
     {
-      "section": "api",
+      "section": "hs",
+      "id": "hs.hsDeprecated",
+      "shortName": "hs.hsDeprecated",
+      "type": "object",
+      "moduleName": "hs",
+      "shortDescription": "a utility component to mark deprecated pieces of code.",
+      "keywords": "bar boolean call code component console create default deprecated foo function generate hs hsdeprecated hsdeprected implementation indicate inject mark message object occured pieces print printmessage prints printtrace produce reporting stack trace true utility var"
+    },
+    {
+      "section": "hs",
+      "id": "hs.object",
+      "shortName": "hs.object",
+      "type": "overview",
+      "moduleName": "hs.object",
+      "shortDescription": "hs.object",
+      "keywords": "$scope _outerfunction adds angular attributes base child children configuration controller female function gender getname hold hs hsconfigurable hsobject html includes inheritance jane john js key-value length male model module myctrl ng-controller ng-repeat object overview parent parent-child people person push register relationship ren return script simple type var"
+    },
+    {
+      "section": "hs",
       "id": "hs.object.HsConfigurable",
       "shortName": "hs.object.HsConfigurable",
       "type": "object",
       "moduleName": "hs.object",
       "shortDescription": "inherits from HsObject and provides for a basic configuration mechanism;",
-      "keywords": "add adds allows angular api attrs basic call callback called calling calls category cb cfg chaining child color completed configuration configure consists console data directive doubles ele example file function green hs hsconfigurable hsexample hsobject html inherits js json jsonfile key key1 key2 label link list literal loading log mechanism method module mylabel myname notation obj obj1 object pairs parameter parameters parent provided range replace replaces restrict result return returns script setup specifies stringify type url value1 value2 var"
+      "keywords": "add adds allows angular attrs basic call callback called calling calls category cb cfg chaining child color completed configuration configure consists console data directive doubles ele example file function green hs hsconfigurable hsexample hsobject html inherits js json jsonfile key key1 key2 label link list literal loading log mechanism method module mylabel myname notation obj obj1 object pairs parameter parameters parent provided range replace replaces restrict result return returns script setup specifies stringify type url value1 value2 var"
     },
     {
-      "section": "api",
+      "section": "hs",
       "id": "hs.object.HsObject",
       "shortName": "hs.object.HsObject",
       "type": "object",
       "moduleName": "hs.object",
       "shortDescription": "Base class for all HelpfulScripts objects.",
-      "keywords": "addition adds api array base called category chaining child children class current function generates helpfulscripts hs hsobject identical list maintaining newname obj1 obj2 object objects paramenter parameter parent provided remove removed removes result retrieves returned returns sets specific string sub-arrays true-ish type unspecified var"
+      "keywords": "_outerfunction adds args arguments array base behavior called category chaining child children class current customize derived directly example function getname gettype helpfulscripts hs hsobject implements jane john list methods__outerfunction newname object objects optional override overview paramenter parent passed person remove removed removes retrieves returned returns setname sets string true true-ish type unspecified var"
     },
     {
-      "section": "api",
+      "section": "hsLayout",
       "id": "hsLayout",
       "shortName": "hsLayout",
       "type": "overview",
       "moduleName": "hsLayout",
-      "shortDescription": "Angular Widget and Layout Library",
-      "keywords": "$interval $scope add allow angular api application attribute automatic background-color border browser center class clicking codebase column columns constraints container controller controls css deliberate directive directives display double eef element enables entire example facilitate factory fafafa ffe fill full-screen function gui height helpful hs-layout hs-moveable hs-rows hs-tiles hs-widget-pane hscolumnslayout hslayout hsrowslayout hstilelayout html https include inside js layout layouts level library live locations manual manually maximize mode module moved myctrl nested ng-controller ng-repeat non-static object overview padding position providing push relative repeated-item resized reversibly row script scripts size solid style surrounding text-align tile tiles toggle top treating var widget widgets window"
+      "shortDescription": "#Angular Widget and Layout Library",
+      "keywords": "$interval $scope add allow angular application attribute automatic background-color border browser center class clicking codebase column columns constraints container controller controls css deliberate directive directives display double eef element enables entire example facilitate factory fafafa ffe fill full-screen function gui height helpful hs-layout hs-moveable hs-rows hs-tiles hs-widget-pane hscolumnslayout hslayout hsrowslayout hstilelayout html https include inside js layout layouts level library live locations manual manually maximize mode module moved myctrl nested ng-controller ng-repeat non-static object overview padding position providing push relative repeated-item resized reversibly row script scripts size solid style surrounding text-align tile tiles toggle top treating var widget widgets window"
     },
     {
-      "section": "api",
+      "section": "hsLayout",
       "id": "hsLayout.controller:hsMoveableCtrl",
       "shortName": "hsMoveableCtrl",
       "type": "controller",
       "moduleName": "hsLayout",
       "shortDescription": "Controller to manage move and size events on widgets.",
-      "keywords": "api controller defined elem element events hs-moveable hslayout manage method move moveable selection size widgets"
+      "keywords": "controller defined elem element events hs-moveable hslayout manage method move moveable selection size widgets"
     },
     {
-      "section": "api",
+      "section": "hsLayout",
       "id": "hsLayout.directive:hsLayout",
       "shortName": "hsLayout",
       "type": "directive",
       "moduleName": "hsLayout",
       "shortDescription": "hs-layout directive. Establishes a space in which",
-      "keywords": "alternative api applies array attributes autmatic avaliable background-color border brackets center class colum columns create creates css default directive eef emphasis establishes fafafa ffe fill height horizontally hs-fill-last-col hs-layout hs-pos hs-size hs-type hs-widget hs-widget-pane hscolumnslayout hslayout hsrelativelayout hsrowslayout hstilelayout html layout live manual marked module myheight object optional options padding position relative remaining required rows sets solid space square stretched style text-align tile tiles type widgets wrapped"
+      "keywords": "alternative applies array attributes autmatic avaliable background-color border brackets center class colum columns create creates css default directive eef emphasis establishes fafafa ffe fill height horizontally hs-fill-last-col hs-layout hs-pos hs-size hs-type hs-widget hs-widget-pane hscolumnslayout hslayout hsrelativelayout hsrowslayout hstilelayout html layout live manual marked module myheight object optional options padding position relative remaining required rows sets solid space square stretched style text-align tile tiles type widgets wrapped"
     },
     {
-      "section": "api",
+      "section": "hsLayout",
       "id": "hsLayout.directive:hsMaximizable",
       "shortName": "hsMaximizable",
       "type": "directive",
       "moduleName": "hsLayout",
       "shortDescription": "adds the maximizable option to a widget or layout. Double clicking the widget will toggle it to fill the entire top-level",
-      "keywords": "adds api background-color center clicking css directive double eee entire ffe fill grid height hs-layout hs-maximizable hs-moveable hs-widget hslayout html layout maximizable maximize module option original padding position relative shrink size space style text-align toggle top-level widget"
+      "keywords": "adds background-color center clicking css directive double eee entire ffe fill grid height hs-layout hs-maximizable hs-moveable hs-widget hslayout html layout maximizable maximize module option original padding position relative shrink size space style text-align toggle top-level widget"
     },
     {
-      "section": "api",
+      "section": "hsLayout",
       "id": "hsLayout.directive:hsMoveable",
       "shortName": "hsMoveable",
       "type": "directive",
       "moduleName": "hsLayout",
-      "shortDescription": "Attributes",
-      "keywords": "allows api area attributes background-color brackets center corner corners css dashboard default determines direction directive eee emphasis ffe grid height horizontal hs-layout hs-moveable hs-size hs-widget hslayout html marked module moved moving number optional padding parameter pixel position relative required resizing scaled sensitive sets size sized snap square steps style supplied text-align top vertical widget widgets wrapped"
+      "shortDescription": "#Attributes",
+      "keywords": "allows area attributes background-color brackets center corner corners css dashboard default determines direction directive eee emphasis ffe grid height horizontal hs-layout hs-moveable hs-widget hslayout html marked module moved moving number optional padding parameter pixel position relative required resizing scaled sensitive sets size sized snap square steps style supplied text-align top vertical widget widgets wrapped"
     },
     {
-      "section": "api",
+      "section": "hsLayout",
       "id": "hsLayout.directive:hsWidget",
       "shortName": "hsWidget",
       "type": "directive",
       "moduleName": "hsLayout",
       "shortDescription": "hs-widget directive. Adds a widget to a layout.",
-      "keywords": "absolute adds api attributes background-color brackets case center clashes command content controller corner corners css dashboard default defaults determined directive ea eee emphasis external ffe file free height hs-include hs-layout hs-moveable hs-pos hs-size hs-widget hs-widget-pane hslayout html inline inserts layout left marked module needed ng-include optional padding position relative required sets size sizes space square statement style supported text-align top transclude true widget wrapped"
+      "keywords": "absolute adds attributes background-color brackets case center clashes command content controller corner corners css dashboard default defaults determined directive ea eee emphasis external ffe file free height hs-include hs-layout hs-moveable hs-pos hs-size hs-widget hs-widget-pane hslayout html inline inserts layout left marked module needed ng-include optional padding position relative required sets size sizes space square statement style supported text-align top transclude true widget wrapped"
     },
     {
-      "section": "api",
+      "section": "hsLayout",
       "id": "hsLayout.object.HsColumnsLayout",
       "shortName": "hsLayout.object.HsColumnsLayout",
       "type": "object",
       "moduleName": "hsLayout.object",
       "shortDescription": "Extends HsLayout.",
-      "keywords": "$interval $scope angular api array attribute attributes background-color border borders center children class column columns container controller create css description directive divides eef elements empty equal evenly extends fafafa ffe fill filled fixed function fw height horizontally hs-columns hs-layout hs-tiles hs-widget-pane hscolumnslayout hslayout html indicators inside js layout leaving left location lw module multiple myctrl n-1 n-2 nested ng-controller ng-repeat object options padding panel partitions position push px relative remaining repeated-item script sequence sets share shared solid space spaced specifications style supported text-align tile top uninterrupted unit unused var w1 w2 widget widgets width widths"
+      "keywords": "$interval $scope angular array attribute attributes background-color border borders center children class column columns container controller create css description directive divides eef elements empty equal evenly extends fafafa ffe fill filled fixed function fw height horizontally hs-columns hs-layout hs-tiles hs-widget-pane hscolumnslayout hslayout html indicators inside js layout leaving left location lw module multiple myctrl n-1 n-2 nested ng-controller ng-repeat object options padding panel partitions position push px relative remaining repeated-item script sequence sets share shared solid space spaced specifications style supported text-align tile top uninterrupted unit unused var w1 w2 widget widgets width widths"
     },
     {
-      "section": "api",
+      "section": "hsLayout",
       "id": "hsLayout.object.HsLayout",
       "shortName": "hsLayout.object.HsLayout",
       "type": "object",
       "moduleName": "hsLayout.object",
       "shortDescription": "Extends HsConfigurable.",
-      "keywords": "abstract api base class don explicitely extends fields functions hs hsconfigurable hslayout instantiations layout obj object options override pos set setwidgetpos setwidgetsize size standard widgets"
+      "keywords": "abstract base class don explicitely extends fields functions hs hsconfigurable hslayout instantiations layout obj object options override pos set setwidgetpos setwidgetsize size standard widgets"
     },
     {
-      "section": "api",
+      "section": "hsLayout",
       "id": "hsLayout.object.HsRelativeLayout",
       "shortName": "hsLayout.object.HsRelativeLayout",
       "type": "object",
       "moduleName": "hsLayout.object",
       "shortDescription": "Extends HsLayout.",
-      "keywords": "api extends hslayout hsrelativelayout layout natural object options places position relative widgets"
+      "keywords": "extends hslayout hsrelativelayout layout natural object options places position relative widgets"
     },
     {
-      "section": "api",
+      "section": "hsLayout",
       "id": "hsLayout.object.HsRowsLayout",
       "shortName": "hsLayout.object.HsRowsLayout",
       "type": "object",
       "moduleName": "hsLayout.object",
       "shortDescription": "Extends HsLayout.",
-      "keywords": "$interval $scope angular api array attribute attributes background-color border borders bottom center children class column container controller create css description directive divides eef elements empty equal evenly extends fafafa ffe fill function fw height heights hs-layout hs-rows hs-tiles hs-widget-pane hslayout hsrowslayout html indicators inside js layout leaving location lw module multiple myctrl n-1 n-2 nested ng-controller ng-repeat object options padding partitions position push px relative remaining repeated-item rows script sequence sets share solid space spaced specifications style supported text-align tile top uninterrupted unit unused var vertically w1 w2 widget widgets"
+      "keywords": "$interval $scope angular array attribute attributes background-color border borders bottom center children class column container controller create css description directive divides eef elements empty equal evenly extends fafafa ffe fill function fw height heights hs-layout hs-rows hs-tiles hs-widget-pane hslayout hsrowslayout html indicators inside js layout leaving location lw module multiple myctrl n-1 n-2 nested ng-controller ng-repeat object options padding partitions position push px relative remaining repeated-item rows script sequence sets share solid space spaced specifications style supported text-align tile top uninterrupted unit unused var vertically w1 w2 widget widgets"
     },
     {
-      "section": "api",
+      "section": "hsLayout",
       "id": "hsLayout.object.HsTileLayout",
       "shortName": "hsLayout.object.HsTileLayout",
       "type": "object",
       "moduleName": "hsLayout.object",
       "shortDescription": "Extends HsLayout.",
-      "keywords": "$interval $scope angular api arranged background-color border bottom center class container controller css equal extend extends fafafa fashion ffe fit fits function grow height horizontally hs-fill-last-col hs-layout hs-tile hs-widget-pane hslayout hstilelayout html js layout left linear module myctrl ng-controller ng-enter ng-enter-active ng-move ng-move-active ng-repeat number object opacity options padding position push relative remnaining repeated-item row script set sized solid square-like style text-align tile tiles top transition var vertically widget widgets width works"
+      "keywords": "$interval $scope angular arranged background-color border bottom center class container controller css equal extend extends fafafa fashion ffe fit fits function grow height horizontally hs-fill-last-col hs-layout hs-tile hs-widget-pane hslayout hstilelayout html js layout left linear module myctrl ng-controller ng-enter ng-enter-active ng-move ng-move-active ng-repeat number object opacity options padding position push relative remnaining repeated-item row script set sized solid square-like style text-align tile tiles top transition var vertically widget widgets width works"
     }
   ],
   "apis": {
-    "api": true
+    "hs": true,
+    "hsLayout": true
   },
   "html5Mode": false,
   "editExample": true,
