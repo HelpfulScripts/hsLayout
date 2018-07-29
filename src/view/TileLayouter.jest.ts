854994974ae7048cfe7f4afb9344eda06eec2b97
hs <>
2018-07-29 12:07:29 -0700

    added coverage and tests

--- src/view/TileLayouter.jest.ts
+++ src/view/TileLayouter.jest.ts
@@ -9,7 +9,7 @@ const layout = (tileSizes: string[]) =>
         css: "myRow",
         content: [`First: ${tileSizes[0]}`, "Second: remainder"]
         };
-        config[type] = tileSizes;
+        config['tiles'] = tileSizes;
         m.mount(o.root, { view: () => m(hslayout.Layout, config) });
         cont = o.root.childNodes[0];
         resolve();
@@ -24,8 +24,8 @@ describe("tiles px", () => {
     const tileSizes = ['150px','100px'];
     beforeAll(() => layout(tileSizes));
     describe("snapshots", () => {
-        // test("rows should be defined", () => expect(cont).toBeDefined());
-        // test(`hs-row-layout, [${tileSizes}]`, () => expect(cont).toMatchSnapshot())
+        test("rows should be defined", () => expect(cont).toBeDefined());
+        test(`hs-row-layout, [${tileSizes}]`, () => expect(cont).toMatchSnapshot());
     });
 });
 
@@ -33,7 +33,7 @@ describe("tiles %", () => {
     const tileSizes = ['30%','25%'];
     beforeAll(() => layout(tileSizes));
     describe("snapshots", () => {
-        // test("rows should be defined", () => expect(cont).toBeDefined());
-        // test(`hs-row-layout, [${tileSizes}]`, () => expect(cont).toMatchSnapshot());
+        test("rows should be defined", () => expect(cont).toBeDefined());
+        test(`hs-row-layout, [${tileSizes}]`, () => expect(cont).toMatchSnapshot());
     });
 });
