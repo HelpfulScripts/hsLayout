d67da6a07cfc14e9b391b8d5b07969583b3d1ce1
hs <helpfulscripts@gmail.com>
2019-09-02 16:50:11 -0700

    added badges to overview

--- src/overview.ts
+++ src/overview.ts
@@ -1,9 +1,18 @@
 /**
- * # hsLayout 
+# hsLayout 
  * 
- * Helpful Scripts UI layout tools. 
- * [`[Github page]`](https://github.com/HelpfulScripts/hsLayout)
- * [`[Coverage Info]`](./data/src/hsLayout/coverage/)
+Helpful Scripts UI layout tools. 
+[`[Coverage Info]`](./data/src/hsLayout/coverage/)
+
+[![npm version](https://badge.fury.io/js/hslayout.svg)](https://badge.fury.io/js/hslayout)
+[![GitHub](https://img.shields.io/badge/GitHub-hsLayout-blue.svg)](https://github.com/helpfulscripts/hslayout)
+[![docs](https://img.shields.io/badge/hsDocs-hsLayout-blue.svg)](https://helpfulscripts.github.io/hsLayout/#!/api/hsLayout/0)
+[![Build Status](https://travis-ci.org/HelpfulScripts/hsLayout.svg?branch=master)](https://travis-ci.org/HelpfulScripts/hsLayout)
+[![Dependencies Status](https://david-dm.org/helpfulscripts/hslayout.svg)](https://david-dm.org/helpfulscripts/hslayout)
+[![Coverage Status](https://coveralls.io/repos/github/HelpfulScripts/hsLayout/badge.svg?branch=master)](https://coveralls.io/github/HelpfulScripts/hsLayout?branch=master)
+[![Known Vulnerabilities](https://snyk.io/test/github/HelpfulScripts/hsLayout/badge.svg?targetFile=package.json)](https://snyk.io/test/github/HelpfulScripts/hsLayout?targetFile=package.json)
+[![NPM License](https://img.shields.io/badge/license-MIT-brightgreen.svg)](https://www.npmjs.com/package/hslayout)
+
  * ___
  * 
  * hsLayout provides means to layout the browser window in various ways.
